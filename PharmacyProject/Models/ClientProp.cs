5cf7dbd163a2b90c4543c84e82a7c8636457261c
Abedallah Radi <abdullahradi620@gmail.com>
2025-12-20 21:13:42 +0300

    add an new class for the cliet

---
+++ PharmacyProject/Models/ClientProp.cs
@@ -0,0 +1,11 @@
+﻿namespace PharmacyProject.Models
+{
+    public class ClientProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
